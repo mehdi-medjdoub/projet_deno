d6c97750fc35270bf2f22f95b333246c7afc452b
Viramouttou Stéphane <stephane.viramouttou@gmail.com>
2021-01-27 20:09:36 +0100

    url secured

--- db/mongodb.ts
+++ db/mongodb.ts
@@ -1,8 +1,10 @@
 import { MongoClient, Bson } from "https://deno.land/x/mongo@v0.21.0/mod.ts";
-//EUFLatxbPC0i8BlM
+import "https://deno.land/x/dotenv/load.ts";
+import { conf } from './config.ts';
+
 const client = new MongoClient();
-await client.connect("mongodb+srv://userdeno:EUFLatxbPC0i8BlM@deno.cdevd.mongodb.net/toast?retryWrites=true&w=majority");
-//await client.connect("mongodb://localhost:27017");
+await client.connect(conf.DB_URL);
+//await client.connect("mongodb://127.0.0.1:27017");
 
 const db = client.database("songs");
 
